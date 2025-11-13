5bb0d736a4c11329a1a31fbaf97e5b5f5cabd86c
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-11-13 03:22:10 +0000

    Add GCS source to `p1` pipeline with CSV format configuration

--- pipelines/p1.py
+++ pipelines/p1.py
@@ -2,4 +2,20 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    OrchestrationSource_1 = SourceTask(
+        task_id = "OrchestrationSource_1", 
+        component = "OrchestrationSource", 
+        kind = "GCSSource", 
+        connector = Connection(kind = "GCS"), 
+        isNew = True, 
+        format = CSVFormat(
+          allowLazyQuotes = False, 
+          allowEmptyColumnNames = True, 
+          separator = ",", 
+          nullValue = "", 
+          encoding = "UTF-8", 
+          header = True
+        ), 
+        fileOperationProperties = {"fileLoadingType" : "filepath", "includeFileNameColumn" : True}
+    )
     p1__Aggregate_0 = Task(task_id = "p1__Aggregate_0", component = "Model", modelName = "p1__Aggregate_0")
