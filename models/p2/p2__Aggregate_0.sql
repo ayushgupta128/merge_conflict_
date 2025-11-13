aa4ad256819ad756d189693a7c61809b1b7cea60
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-11-13 02:55:51 +0000

    Add `p2` pipeline with new SQL macros and transformations - Add `generate_schema_name` and `prophecy_tmp_source` SQL macros - Add `p2__Aggregate_0` SQL transformation in `p2` pipeline

---
+++ models/p2/p2__Aggregate_0.sql
@@ -0,0 +1,19 @@
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "ayush_demos",
+    "schema": "demos"
+  })
+}}
+
+WITH Aggregate_0 AS (
+
+  SELECT * 
+  
+  FROM `` AS in0
+
+)
+
+SELECT *
+
+FROM Aggregate_0
