30c1a5d0f5b70a69212979b502f2129d56cbbdfd
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-11-13 06:57:47 +0000

    Add `send_email` gem to `p2` pipeline for email notifications

--- pipelines/p2.py
+++ pipelines/p2.py
@@ -5,3 +5,15 @@ with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
     p2__Aggregate_0 = Task(task_id = "p2__Aggregate_0", component = "Model", modelName = "p2__Aggregate_0")
     p2__XMLParse_1 = Task(task_id = "p2__XMLParse_1", component = "Model", modelName = "p2__XMLParse_1")
     Directory_1 = Task(task_id = "Directory_1", component = "Directory", integration = "", path = "")
+    send_email = Task(
+        task_id = "send_email", 
+        component = "Email", 
+        body = "", 
+        subject = "", 
+        includeData = False, 
+        fileName = "", 
+        to = ["gupta@gmail.com"], 
+        connection = Connection(kind = "smtp", id = "smtp_1"), 
+        fileFormat = "", 
+        hasTemplate = False
+    )
