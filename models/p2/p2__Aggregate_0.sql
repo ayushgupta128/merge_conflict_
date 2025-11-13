a7f526d7978463ab74735b08397b14112ce8d6a1
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-11-13 08:12:13 +0000

    Refactor `p2` pipeline SQL transformations - Convert `p2__Aggregate_0` and `p2__XMLParse_1` to ephemeral models - Remove DAG and email orchestration logic from SQL files

--- models/p2/p2__Aggregate_0.sql
+++ models/p2/p2__Aggregate_0.sql
@@ -1,19 +1,19 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "ayush_demos",
+    "schema": "demos"
+  })
+}}
 
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    p2__Aggregate_0 = Task(task_id = "p2__Aggregate_0", component = "Model", modelName = "p2__Aggregate_0")
-    p2__XMLParse_1 = Task(task_id = "p2__XMLParse_1", component = "Model", modelName = "p2__XMLParse_1")
-    Directory_1 = Task(task_id = "Directory_1", component = "Directory", integration = "", path = "")
-    send_email = Task(
-        task_id = "send_email", 
-        component = "Email", 
-        body = "", 
-        subject = "", 
-        includeData = False, 
-        fileName = "", 
-        to = ["ayush@gmail.com"], 
-        connection = Connection(kind = "smtp", id = "smtp_1"), 
-        fileFormat = "", 
-        hasTemplate = False
-    )
+WITH Aggregate_0 AS (
+
+  SELECT * 
+  
+  FROM `` AS in0
+
+)
+
+SELECT *
+
+FROM Aggregate_0
--- models/p2/p2__XMLParse_1.sql
+++ models/p2/p2__XMLParse_1.sql
@@ -1,19 +1,17 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "ayush_demos",
+    "schema": "demos"
+  })
+}}
 
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    p2__Aggregate_0 = Task(task_id = "p2__Aggregate_0", component = "Model", modelName = "p2__Aggregate_0")
-    p2__XMLParse_1 = Task(task_id = "p2__XMLParse_1", component = "Model", modelName = "p2__XMLParse_1")
-    Directory_1 = Task(task_id = "Directory_1", component = "Directory", integration = "", path = "")
-    send_email = Task(
-        task_id = "send_email", 
-        component = "Email", 
-        body = "", 
-        subject = "", 
-        includeData = False, 
-        fileName = "", 
-        to = ["ayush@gmail.com"], 
-        connection = Connection(kind = "smtp", id = "smtp_1"), 
-        fileFormat = "", 
-        hasTemplate = False
-    )
+WITH XMLParse_1 AS (
+
+  {{ prophecy_basics.XMLParse('', '', 'parseFromSampleRecord', '', '') }}
+
+)
+
+SELECT *
+
+FROM XMLParse_1
