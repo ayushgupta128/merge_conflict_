7959feba023b8ea443466e3ebe24970b6b446fda
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-11-14 06:21:22 +0000

    Add datasets `tile`, `customer`, and `new_sales` to `p2` pipeline and define `ayush_demos.demos` source

--- pipelines/p2.py
+++ pipelines/p2.py
@@ -2,6 +2,16 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    tile = Task(
+        task_id = "tile", 
+        component = "Dataset", 
+        table = {"name" : "tile", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
+    )
+    customer = Task(
+        task_id = "customer", 
+        component = "Dataset", 
+        table = {"name" : "customer", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
+    )
     p2__Aggregate_0 = Task(task_id = "p2__Aggregate_0", component = "Model", modelName = "p2__Aggregate_0")
     p2__XMLParse_1 = Task(task_id = "p2__XMLParse_1", component = "Model", modelName = "p2__XMLParse_1")
     Directory_1 = Task(task_id = "Directory_1", component = "Directory", integration = "", path = "")
@@ -16,4 +26,9 @@ with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
         connection = Connection(kind = "smtp", id = "smtp_1"), 
         fileFormat = "", 
         hasTemplate = False
+    )
+    new_sales = Task(
+        task_id = "new_sales", 
+        component = "Dataset", 
+        table = {"name" : "new_sales", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
     )
